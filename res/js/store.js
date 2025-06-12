ba07f8dc7d218b4b4b4dd4c2c59b701e2efa95e6
ibrahim-younan-1997 <ibrahimyounan1997@gmail.com>
2025-06-12 20:33:51 +0300

    update store.js

---
+++ res/js/store.js
@@ -0,0 +1,50 @@
+const store = PetiteVue.reactive({
+/*________________________________________________________________*/
+
+
+imgSrc: "/res/pics/tours/",
+
+tours: [
+	
+{
+	id: "pyramids",
+	title: "Pyramids",
+	description: `
+	Don't miss the chance to step back in time to ancient Egyptian history. Explore one of the 7 wonders of the world: The great Pyramids and sphinx, with a trip to the National Egyptian Museum, home to over 4000 years of history including the contents of Tutankhamun's Tomb.
+Followed by a small boat tour on the river Nile.
+Lunch is included.
+`,
+	imgs: [	
+		"IMG_2278.WEBP",
+		"IMG_2280.WEBP",
+		"IMG_2280.WEBP",
+		"IMG_2280.WEBP",
+		"IMG_2280.WEBP"
+		],
+},
+
+
+
+
+
+
+{
+	id: "submarin",
+	title: "submarin",
+	description: `
+	diving
+	`,
+	imgs: [
+		"IMG_2280.WEBP"
+	],
+},
+  
+
+  
+  ],
+/*________________________________________________________________*/
+
+
+
+/*________________________________________________________________*/
+});
